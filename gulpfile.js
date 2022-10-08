43840a36f48ad25c189415d0eb6e8fc349a19cfb
ShineShao <xiaoshaoqq@gmail.com>
2022-10-08 22:14:02 +0800

    perf: compress css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const cleanCss = require("gulp-clean-css");
 const del = require("del");
 
 const srcDir = "./src";
@@ -15,18 +16,30 @@ function buildStyle() {
       ignore: ["**/demos/**/*", "**/tests/**/*"]
     })
     .pipe(sass().on("error", sass.logError))
+    .pipe(cleanCss())
     .pipe(gulp.dest("./lib"));
 }
 
-function copyAssets() {
+function copyPng2Lib() {
   return gulp
-    .src([`${srcDir}/**/*.png`, `${srcDir}/**/*.html`, `${srcDir}/**/*.scss`])
-    .pipe(gulp.dest("./lib"))
-    .pipe(gulp.dest("./docs"));
+    .src([`${srcDir}/**/*.png`, `${srcDir}/**/*.scss`])
+    .pipe(gulp.dest("./lib"));
+}
+
+function copyHtml2Docs() {
+  return gulp.src([`${srcDir}/**/*.html`]).pipe(gulp.dest("./docs"));
 }
 
 function generateDocs() {
-  return gulp.src([`./lib/**/*`]).pipe(gulp.dest("./docs"));
+  return gulp
+    .src([`./lib/**/*.css`, `./lib/**/*.png`])
+    .pipe(gulp.dest("./docs"));
 }
 
-exports.default = gulp.series(clean, buildStyle, copyAssets, generateDocs);
+exports.default = gulp.series(
+  clean,
+  buildStyle,
+  copyPng2Lib,
+  copyHtml2Docs,
+  generateDocs
+);
