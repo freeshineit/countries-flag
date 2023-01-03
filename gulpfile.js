ec25fcd9fdcb518145c37671ef3a3340f58e2815
ShineShao <xiaoshaoqq@gmail.com>
2023-01-03 20:17:11 +0800

    feat: compress html

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const cleanCss = require("gulp-clean-css");
+const htmlMin = require("gulp-htmlmin"); //引入html压缩模块
 const del = require("del");
 
 const srcDir = "./src";
@@ -27,7 +28,21 @@ function copyPng2Lib() {
 }
 
 function copyHtml2Docs() {
-  return gulp.src([`${srcDir}/**/*.html`]).pipe(gulp.dest("./docs"));
+  return gulp
+    .src([`${srcDir}/**/*.html`])
+    .pipe(
+      htmlMin({
+        removeComments: true, //清除HTML注释
+        collapseWhitespace: true, //压缩HTML
+        collapseBooleanAttributes: true, //省略布尔属性的值 <input checked="true"/> ==> <input checked />
+        // removeEmptyAttributes: true, //删除所有空格作属性值 <input id="" /> ==> <input />
+        // removeScriptTypeAttributes: false, //删除<script>的type="text/javascript"
+        // removeStyleLinkTypeAttributes: true, //删除<style>和<link>的type="text/css"
+        minifyJS: true, //压缩页面JS
+        minifyCSS: true //压缩页面CSS
+      })
+    )
+    .pipe(gulp.dest("./docs"));
 }
 
 function generateDocs() {
